a4c475c532cd3a31570b9b326e1eaee9ce50347b
Christopher Drury <gamepro65@new-host-2.home>
2011-04-25 01:38:29 -0400

    More comments and general fixes

--- Asteroid.java
+++ Asteroid.java
@@ -12,25 +12,46 @@ import processing.core.PApplet;
 
 
 public class Asteroid extends AbstractEntity{
-
-	public Vector2f driftSpeed;
-	int size;
-	int health;
 	
+	//Public because I'm the only one coding this game
+	//And its quicker than making helper methods.
+	public Vector2f driftSpeed;	
+	public int size;
+	public int health;
+	
+	
+	//Default constructor of our asteroid
 	public Asteroid(PApplet aParent, Vector2f aPosition, int aSize)
 	{
+		//Tell our abstract entity parent the information it needs
 		super(aParent, aPosition);
+		//Psudeo random number generator. Used for our random
+		//Asteroid creation
 		Random ran = new Random();
 		size = aSize;
-		verticiesSizes = new ArrayList();		
+		//Minimum number of sides is 5
+		//Triangles don't look like asteroids
 		numberOfSides = (int)(ran.nextDouble() * 10)+5;
+		//Health per asteroid is 3
 		health = 3;
+		//Random angle for our new asteroid to drift
 		double randomAngle = ran.nextDouble()*360;
+		//set the drift speed of our asteroid
+		//The speed varies from 0-180 pixel/s
 		driftSpeed = new Vector2f((float)(Math.cos(Math.toRadians(randomAngle))*ran.nextDouble()*180), (float)(Math.sin(Math.toRadians(randomAngle))*ran.nextDouble()*180));
+		//Randomly create our asteroid. We do 360 - one angle of our side
+		//This is because we dont need to remake our 360 degree point as its
+		//our 0 point as well. Then using the number of sides in our asteroid
+		//Loop in a circle around our point creating random distances
+		//for each angle.
 		for (int x = 0; x < 360-(360/(numberOfSides)); x+= (360/(numberOfSides)))
 		{
-			double randomSize = (ran.nextDouble() * size-10)+15;
+			//Distance from center is created randomly between
+			//30 and the largest size it could be - 10
+			double randomSize = (ran.nextDouble() * size-10)+30;
+			//Array lists only store objects, not primitives
 			Double mySize = new Double(randomSize);
+			//Add distance to my array of distances.
 			verticiesSizes.add(mySize);
 		}
 	}
@@ -38,13 +59,21 @@ public class Asteroid extends AbstractEntity{
 
 	public void update(double aDelta)
 	{
+		//Update our parent to update our position information
 		super.update(aDelta);
+		//Drift the asteroid. This is set
+		//To drift speed * delta to make a smooth move
+		//Then * difficulty making it harder later because
+		//The asteroids move faster.
 		position.x += (float)(driftSpeed.x * Main.difficulty * aDelta);
 		position.y += (float)(driftSpeed.y * Main.difficulty * aDelta);
 	}
 		
 	public void draw()
 	{	
+		//No special drawing attributes to our asteroids
+		//Just draw our verticies. Our parent does this
+		//for us.
 		super.draw();		
 	}
 
--- Bullets.java
+++ Bullets.java
@@ -5,9 +5,10 @@ import processing.core.PApplet;
 public class Bullets {
 	
 	double bulletx, bullety, crosshairx, crosshairy;
-	double speedx, speedy, bulletLife = 60;
+	double speedx, speedy;
 	PApplet parent;
 	
+	//Default constructor
 	public Bullets(PApplet papp, int x, int y, int crosshairx, int crosshairy, int speed)
 	{
 		parent = papp;
@@ -19,6 +20,9 @@ public class Bullets {
 		double angleAradians = Math.atan2(ydis,xdis);
 		double rotation = Math.toDegrees(angleAradians);
 		//creates the points to add to next frame
+		//This is a way of presetting our increments
+		//That way we dont have to do more math fucntions
+		//Each cycle.
 		speedx = (float)(speed * 60 * Math.cos(Math.toRadians(rotation)));
 		speedy = (float)(speed * 60 * Math.sin(Math.toRadians(rotation)));
 		
@@ -26,13 +30,19 @@ public class Bullets {
 	}
 	public void update(double aDelta)
 	{
+		//Increment our location times delta
+		//To make smooth transitions
 		bulletx += speedx * aDelta;
 		bullety += speedy * aDelta;
 	}
 	
 	public void draw()
 	{
+		//Fill the rect with a greyish white.
 		parent.fill(155);
+		//render rect in the center of the pisition.
+		parent.rectMode(parent.CENTER);
+		//draw rect
 		parent.rect((float)bulletx, (float)bullety, 3, 3);
 		
 	}
